ba0a3de502e0d66b173718ea4051e2842befd8f6
Shawn <ShawnYeong1016@gmail.com>
2024-12-02 18:48:41 +0800

    React Resume

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+function Experience() {
+  return (
+    <>
+    <h1>Software Developer</h1>
+    <h4>Sigma Lab Sdn Bhd</h4>
+    </>
+  )
+}
+
+
+function App() {
+  
+  return (
+    <>
+    <h1>Shawn</h1>
+    <h2>Software Developer</h2>
+    <Experience />
+    </>
+  )
+}
+
+export default App
+
+
+
+
